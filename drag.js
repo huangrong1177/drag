ab32f5d164ddb563acf18f9d252ea70dcb174b03
huangrong1177 <huangrong1177@163.com>
2016-05-08 23:06:10 +0800

    change

--- drag.js
+++ drag.js
@@ -2,7 +2,8 @@ function Drag(id){
 	this.oDiv = document.getElementById("id");
 }
 
-Drag.
-document.onmousedown = function(ev){
+Drag.prototype.init() = function(){
+	document.onmousedown = function(ev){
 
+	}
 }
